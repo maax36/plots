015e29e499cfad14426e7dbf48aac44533135ae3
maax36 <freeivi8888@gmail.com>
2023-06-24 22:37:57 +0300

    update

---
+++ cfg/general.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+// db
+const DB_HOST = 'localhost';
+const DB_USER = 'u2113733_default';
+const DB_PASS = '1Y8QQPT5mf3OyIzc';
+const DB_NAME = 'u2113733_default';
+// common
+const SITE_SCHEME = 'http';
+const DEFAULT_TIMEZONE = '240';
+const LOGIN_ATTEMPTS = '15';
